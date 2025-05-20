74463e910296cdd430b161db10d4f23f9b46beb2
asfahulmuhib <asfahulmuhib@gmail.com>
2025-05-20 07:00:53 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from '@/context/LanguageContext';
-import Home from './pages/Home';
-import Terms from './pages/Terms';
-import Privacy from './pages/Privacy';
-import Cookies from './pages/Cookies';
-import PaymentSuccessful from './pages/PaymentSuccessful';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import PaymentSuccessfull from "./pages/PaymentSuccessfull";
+import Terms from "./pages/Terms"; // Impor halaman Terms
+import Privacy from "./pages/Privacy"; // Impor halaman Privacy
+import Cookies from "./pages/Cookies"; // Impor halaman Cookies
+import { LanguageProvider } from "./context/LanguageContext";
+import "./App.css";
 
-const App = () => {
-  return (
-    <LanguageProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/cookies" element={<Cookies />} />
-          <Route path="/paymentsuccessfull" element={<PaymentSuccessful />} />
-        </Routes>
-      </BrowserRouter>
-    </LanguageProvider>
-  );
-};
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <LanguageProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/paymentsuccessfull" element={<PaymentSuccessfull />} />
+            <Route path="/terms" element={<Terms />} /> {/* Route untuk Terms */}
+            <Route path="/privacy" element={<Privacy />} /> {/* Route untuk Privacy */}
+            <Route path="/cookies" element={<Cookies />} /> {/* Route untuk Cookies */}
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </LanguageProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
