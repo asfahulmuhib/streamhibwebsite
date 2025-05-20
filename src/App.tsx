d9de03a16eddea26f8d42a7306675fa111f0bc3e
asfahulmuhib <asfahulmuhib@gmail.com>
2025-05-20 07:31:53 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import PaymentSuccessfull from "./pages/PaymentSuccessfull";
-import Terms from "./pages/Terms"; // Impor halaman Terms
-import Privacy from "./pages/Privacy"; // Impor halaman Privacy
-import Cookies from "./pages/Cookies"; // Impor halaman Cookies
+import Terms from "./pages/Terms";
+import Privacy from "./pages/Privacy";
+import Cookies from "./pages/Cookies";
+import Tutorial from "./pages/Tutorial"; // Impor Tutorial
 import { LanguageProvider } from "./context/LanguageContext";
 import "./App.css";
 
@@ -24,10 +25,10 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/paymentsuccessfull" element={<PaymentSuccessfull />} />
-            <Route path="/terms" element={<Terms />} /> {/* Route untuk Terms */}
-            <Route path="/privacy" element={<Privacy />} /> {/* Route untuk Privacy */}
-            <Route path="/cookies" element={<Cookies />} /> {/* Route untuk Cookies */}
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/cookies" element={<Cookies />} />
+            <Route path="/tutorial" element={<Tutorial />} /> {/* Route untuk Tutorial */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
